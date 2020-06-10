b29a3f071d869d73e746904892f7869cd35bf6c5
ntrubnikova <natalia.trubnikova@gmail.com>
2020-06-10 18:00:14 +0300

    Add font specimens

---
+++ fonts/js/scripts.js
@@ -0,0 +1,14 @@
+var textsample = 
+
+"<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.</p>" +
+		
+" <ul><li>Cras faucibus condimentum odio. Sed ac ligula. Aliquam at eros.</li><li>Etiam at ligula et tellus ullamcorper ultrices.</li></ul>" +
+		
+"<p>Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien. Donec ut est in lectus consequat consequat. Etiam eget dui. Aliquam erat volutpat. Sed at lorem in nunc porta tristique. Proin nec augue. Quisque aliquam tempor magna.</p>" +
+		
+"<ol><li>Etiam at ligula et tellus ullamcorper ultrices. In fermentum, lorem non cursus porttitor, diam urna accumsan lacus, sed interdum wisi nibh nec nisl. </li><li>Ut tincidunt volutpat urna.</li><li>Mauris eleifend nulla eget mauris. Sed cursus quam id felis. Curabitur posuere quam vel nibh.</li></ol>" +
+		
+"<p>Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Integer nulla. Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien. Donec ut est in lectus consequat consequat. Etiam eget dui. Aliquam erat volutpat. Sed at lorem in nunc porta tristique.</p>"
+
+$('.text').html(textsample);
+$('example>div:odd').addClass('shaded');
